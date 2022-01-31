fe91a3ec3bef070b65232f7fbc5f19effb80e209
Najeeb Arif <NajeebArif786@outlook.com>
2022-01-31 18:38:47 +0530

    Add helper methods in Message Server

--- src/message-server.ts
+++ src/message-server.ts
@@ -3,6 +3,7 @@ import * as WebSocket from "ws";
 export abstract class MessageServer<T> {
   constructor(private readonly wsServer: WebSocket.Server) {
     this.wsServer.on("connection", this.subscribeToMessages);
+    this.wsServer.on("error", this.cleanupDeadClients);
   }
 
   protected abstract handleMessage(sender: WebSocket, message: T): void;
@@ -16,4 +17,38 @@ export abstract class MessageServer<T> {
       }
     });
   };
+
+  private readonly cleanupDeadClients = (): void => {
+    this.wsServer.clients.forEach((client) => {
+      if (this.isDead(client)) {
+        this.wsServer.clients.delete(client);
+      }
+    });
+  };
+
+  protected broadcastExcept(
+    currentClient: WebSocket,
+    message: Readonly<T>
+  ): void {
+    this.wsServer.clients.forEach((client) => {
+      if (!this.isDead(client) && client !== currentClient) {
+        client.send(JSON.stringify(message));
+      }
+    });
+  }
+
+  protected replyTo = (client: WebSocket, message: Readonly<T>): void => {
+    client.send(JSON.stringify(message));
+  };
+
+  protected get clients(): Set<WebSocket> {
+    return this.wsServer.clients;
+  }
+
+  private isDead(client: WebSocket): boolean {
+    return (
+      client.readyState === WebSocket.CLOSING ||
+      client.readyState === WebSocket.CLOSED
+    );
+  }
 }
